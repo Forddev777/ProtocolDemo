3887aeeea16690e65de24b11949428b8871fe573
Forddev777 <taothongdev@gmail.com>
2022-05-30 22:52:54 +0700

    update
    update

--- ProtocolDemo/main.swift
+++ ProtocolDemo/main.swift
@@ -1,3 +1,8 @@
+protocol CanFly {
+    func fly()
+}
+
+
 class Bird {
 
     var isFemale = true
@@ -9,21 +14,19 @@ class Bird {
             
     }
         
-    
-    func fly(){
-            print("THe bird flaps its wins and lifts off into the sky.")
-    }
+   
 }
 
 
 // new class Eagle inheritance for Class Bird //
-class Eagle: Bird {
-    
+class Eagle: Bird , CanFly {
+    func fly() {
+        print("The eagle flaps its wings and lifts off into the sky.")
+    }
     
     func soar(){
             print("The eagle glides in the air using air currents.")
     }
-    
 }
 
 //  create  myEagle   = Eagle()
@@ -31,14 +34,13 @@ class Eagle: Bird {
 let myEagle = Eagle()
 
 // start require
-myEagle.fly()
-myEagle.layEgg()
-myEagle.soar()
+    //myEagle.fly()
+    //myEagle.layEgg()
+    //myEagle.soar()
 // end require
  
 
 class  Penquin: Bird {
-       
     func swim(){
             print("The penguin paddels through the water ")
     }
@@ -51,9 +53,28 @@ class  Penquin: Bird {
 let myPenguin = Penquin()
 
 // start require
-myPenguin.swim()
-myPenguin.layEgg()
+    //myPenguin.swim()
+    //myPenguin.layEgg()
 // end require
 
 
+
+struct FlyingMuseum {
+    func flyingDemo(flyingObject: CanFly ){
+        flyingObject.fly()
+    }
+}
+
+let museum = FlyingMuseum()
+museum.flyingDemo(flyingObject: myplane )
+
+
+struct   Airplane: CanFly  {
+     func fly(){
+            print("The airplane uses its engine to lift off into the air.")
+    }
+}
+
+
+let myplane = Airplane()
 
