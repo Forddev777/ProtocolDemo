645778bbbfc0fab598ad4c1ad72670018eaa9bc3
Forddev777 <taothongdev@gmail.com>
2022-05-30 22:17:00 +0700

    Learn Protocol in swift
    Learn Protocol in swift

--- ProtocolDemo/main.swift
+++ ProtocolDemo/main.swift
@@ -1,11 +1,59 @@
-//
-//  main.swift
-//  ProtocolDemo
-//
-//  Created by suriya taothongkom on 30/5/2565 BE.
-//
+class Bird {
 
-import Foundation
+    var isFemale = true
+    
+    func layEgg() {
+        if isFemale {
+                print("The bird makes a new bird in a  shell ")
+        }
+            
+    }
+        
+    
+    func fly(){
+            print("THe bird flaps its wins and lifts off into the sky.")
+    }
+}
 
-print("Hello, World!")
+
+// new class Eagle inheritance for Class Bird //
+class Eagle: Bird {
+    
+    
+    func soar(){
+            print("The eagle glides in the air using air currents.")
+    }
+    
+}
+
+//  create  myEagle   = Eagle()
+
+let myEagle = Eagle()
+
+// start require
+myEagle.fly()
+myEagle.layEgg()
+myEagle.soar()
+// end require
+ 
+
+class  Penquin: Bird {
+       
+    func swim(){
+            print("The penguin paddels through the water ")
+    }
+    
+}
+
+
+//  create  myEagle   = Eagle()
+
+let myPenguin = Penquin()
+
+// start require
+myPenguin.swim()
+myPenguin.layEgg()
+// end require
+
+
 
